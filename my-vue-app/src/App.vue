e07c8343c61cd4e7d67f9840b2d03e1a24ac7bf2
SourTaras <velgantaras03@gmail.com>
2025-07-31 18:50:39 +0300

    vue

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <HeaderComponent title="Вітаю у Vue-додатку!" />
+  <MessageComponent title="Привіт,Vue" message="Я вмію створювати компоненти із пропсами!" />
+  <MessageComponent title="Що далі" message="Тепер я можу передавати різні дані в компоненти!" />
+  </div>
+</template>
+<script>
+import HeaderComponent from "./components/Header.vue"
+import MessageComponent from "./components/MessageComponent.vue"
+
+export default {
+  components: {
+    HeaderComponent,
+    MessageComponent
+  }
+}
+</script>
+
---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+<header>
+<h1>{{ title }}</h1>
+</header>
+</template>
+<script>
+export default {
+    name: "HeaderComponent",
+    props: {
+        title:{
+            type: String,
+            required:true
+        }
+        
+    }
+}
+</script>
+<style scoped>
+header{
+    background: #42b983;
+    color: white;
+    padding: 15px;
+    text-align:center;
+}
+</style>
