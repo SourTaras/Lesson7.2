afd8d886e0e8d5a90b724634b7f56fd1cef6b058
SourTaras <velgantaras03@gmail.com>
2025-08-04 17:25:32 +0300

    homework

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -3,16 +3,19 @@
   <HeaderComponent title="Вітаю у Vue-додатку!" />
   <MessageComponent title="Привіт,Vue" message="Я вмію створювати компоненти із пропсами!" />
   <MessageComponent title="Що далі" message="Тепер я можу передавати різні дані в компоненти!" />
+  <BusinessComponent name="Володимир" phone="380929433784" email="example@mail.com" />
   </div>
 </template>
 <script>
 import HeaderComponent from "./components/Header.vue"
 import MessageComponent from "./components/MessageComponent.vue"
+import BusinessComponent from "./components/Business.vue
 
 export default {
   components: {
     HeaderComponent,
-    MessageComponent
+    MessageComponent,
+    BusinessComponent
   }
 }
 </script>
---
+++ my-vue-app/src/components/Business.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<ul>
+<li>{{ name }} </li>
+<li>{{ phone }} </li>
+<li>{{ email }} </li>
+</ul>
+</div>
+</template>
+<script>
+export default {
+    name: "BusinessComponent",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: Number,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
